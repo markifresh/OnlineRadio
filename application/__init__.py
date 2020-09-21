1fba1375dd986b89512e4fac3dd8bbb6232272de
Mark Shteiman <markifresh@gmail.com>
2020-09-21 13:04:41 +0300

    change structure of app

--- application/__init__.py
+++ application/__init__.py
@@ -1,1 +1,31 @@
-from werkzeug.utils import cached_property
+
+from flask import Flask, _app_ctx_stack
+# from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import scoped_session
+from application.db_models.extenders_for_db_models import db_session
+from application.blueprints import radios_page
+from application.apis.auth import oauth
+
+
+# Globally accessible libraries
+#db = SQLAlchemy()
+
+def create_app(confConfClass):
+    """Initialize the core application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object(confConfClass)
+
+    # Initialize Plugins
+    #db.init_app(app)
+    app.session = scoped_session(db_session, scopefunc=_app_ctx_stack.__ident_func__)
+    with app.app_context():
+        # # Include our Routes
+        # from . import routes
+        #
+        # # Register Blueprints
+        # app.register_blueprint(auth.auth_bp)
+        # app.register_blueprint(admin.admin_bp)
+        app.register_blueprint(radios_page.radios_page)
+        app.register_blueprint(oauth.oauth_api)
+
+        return app
--- config.py
+++ config.py
@@ -1,24 +1,67 @@
-db_location = 'test2.sqlite'
-exclude_radios = ['FIP_REGGAE', 'FIP_METAL']
-url_api_open_radio = 'https://openapi.radiofrance.fr/v1/graphql'
+from dotenv import load_dotenv
+from pathlib import Path
+from os import getenv
 
-djam_radio = {'id': 'DJAM',
-              'url': 'https://www.djamradio.com',
-              'tracks_request_url': 'https://www.djamradio.com/actions/retrieve.php'}
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+class Config:
+    """Base config."""
+    SECRET_KEY = getenv('SECRET_KEY')
+    SESSION_COOKIE_NAME = getenv('SESSION_COOKIE_NAME')
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-fip_radio = {'id': 'FIP',
-             'url': 'https://www.fip.fr/',
-             'tracks_request_url': url_api_open_radio}
+class ProdConfig(Config):
+    FLASK_ENV = 'production'
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = f'sqlite:///{getenv("PROD_DATABASE_URI")}'
+    DATABASE_ECHO = False
+    PORT = 8080
+    # SERVER_NAME = 'prodradio.localdomain'
+    HOST = '0.0.0.0'
 
-all_environments = {
-    "development": {
-        "port": 5000,
-        "debug": True,
-        "swagger-url": "/api/swagger"},
 
-    "production": {
-        "port": 8080,
-        "debug": False,
-        "swagger-url": None}
-}
+class DevConfig(Config):
+    FLASK_ENV = 'development'
+    DEBUG = True
+    TESTING = True
+    DATABASE_URI = f'sqlite:///{getenv("DEV_DATABASE_URI")}'
+    DATABASE_ECHO = True
+    PORT = 5000
+    # SERVER_NAME = 'devradio.localdomain'
+    HOST = '127.0.0.1'
+
+
+
+class APIConfig:
+    radio_api_key = getenv('radio_api_key')
+    spotify_api_id = getenv('spotify_api_id')
+    spotify_api_key = getenv('spotify_api_key')
+    youtube_api_key = getenv('youtube_api_key')
+
+
+class RadioConfig:
+    exclude_radios = ['FIP_REGGAE', 'FIP_METAL']
+    url_api_open_radio = 'https://openapi.radiofrance.fr/v1/graphql'
+
+    djam_radio = {'id': 'DJAM',
+                  'url': 'https://www.djamradio.com',
+                  'tracks_request_url': 'https://www.djamradio.com/actions/retrieve.php'}
+
+    fip_radio = {'id': 'FIP',
+                 'url': 'https://www.fip.fr/',
+                 'tracks_request_url': url_api_open_radio}
+
+class OauthSpotifyConfig:
+    SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+    SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+    SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+    SPOTIFY_API_VERSION = "v1"
+    SPOTIFY_API_URL = f"{SPOTIFY_API_BASE_URL}/{SPOTIFY_API_VERSION}"
+    SCOPE = "playlist-modify-public playlist-modify-private"
+    CLIENT_ID = APIConfig.spotify_api_id
+    CLIENT_KEY = APIConfig.spotify_api_key
