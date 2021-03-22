8e882ef69d18976371a83c883c4ec7daa0a0a704
Mark Shteiman <markifresh@gmail.com>
2021-03-22 23:39:05 +0200

    implement radios by interfaces; add worker for Radios;

--- run_app.py
+++ run_app.py
@@ -7,3 +7,7 @@ app = create_app(cur_conf)
 if __name__ == "__main__":
     print(app.url_map)
     app.run(host=cur_conf.HOST)
+
+
+# pip3 freeze > requirements.txt
+# pipenv run pip freeze > requirements.txt
