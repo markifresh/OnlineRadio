80f5862745b52975a344d701d93fb2362d29920f
Mark Shteiman <markifresh@gmail.com>
2021-03-31 21:52:04 +0300

    modification user DB

--- application/db_models/user.py
+++ application/db_models/user.py
@@ -1,27 +1,52 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from application.db_models.extenders_for_db_models import BaseExtended
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, JSON
 from datetime import datetime
 
 class User(UserMixin, BaseExtended):
 
-    __tablename__ = 'login-users'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False, unique=False)
-    email = Column(String(40), unique=True, nullable=False)
-    password = Column(String(200), primary_key=False, unique=False, nullable=False)
-    created_on = Column(DateTime, default=datetime.now)
-    last_login = Column(DateTime, index=False, unique=False, nullable=True)
+    created_on = Column(DateTime(), default=datetime.now)
+    accounts = Column(JSON)
+    last_login = Column(DateTime)
 
-    def set_password(self, password):
-        """Create hashed password."""
-        self.password = generate_password_hash(password, method='sha256')
+    @classmethod
+    def get_user_id(cls, session_dict):
+        ms_service = session_dict['ms_service']
+        ms_username = session_dict['ms_user']['display_name']
+        ms_user_id = session_dict['ms_user']['id']
+        db_user = cls.query(cls).filter(cls.accounts.contains({ms_service: {
+                  'display_name': ms_username,
+                  'id': ms_user_id}}))
 
-    def check_password(self, password):
-        """Check hashed password."""
-        return check_password_hash(self.password, password)
+        if db_user.count() == 0:
+            cls.commit_data({
+                'accounts': {
+                    ms_service: {
+                        'display_name': ms_username,
+                        'id': ms_user_id,
+                        'last_login': datetime.now()
+                    }}})
+
+        # todo: replace update with working one
+        elif db_user.count() == 1:
+            db_user.update({'last_login': datetime.now()})
+
+        else:
+            return {'success': False, 'result': 'few matches'}
+
+        return {'success': True, 'result': ''}
+
+    # def set_password(self, password):
+    #     """Create hashed password."""
+    #     self.password = generate_password_hash(password, method='sha256')
+    #
+    # def check_password(self, password):
+    #     """Check hashed password."""
+    #     return check_password_hash(self.password, password)
 
     def __repr__(self):
-        return f'<User {self.name}>'
+        return f'<User {self.id}>'
